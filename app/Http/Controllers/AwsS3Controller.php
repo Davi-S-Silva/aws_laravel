becd55d964f4715b0feaf9d27ab9c95dc382c122
davi-s-silva <daviedeus43@gmail.com>
2023-09-29 21:51:45 -0300

    install

---
+++ app/Http/Controllers/AwsS3Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Services\StorageService;
+
+use Illuminate\Http\Request;
+
+class AwsS3Controller extends Controller
+{
+    public function index(){
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $url = 'https://s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/' . env('AWS_BUCKET') . '/';
+    try {
+        Storage::disk('s3')->put(
+            'files\teste.jpg',
+            file_get_contents('C:\Users\davie\Downloads\teste.jpg')
+        );
+
+        return ('Image uploaded successfully');
+    } catch (Exception $exception) {
+        echo "Failed to upload $file_name with error: " . $exception->getMessage();
+        exit("Please fix error with file upload before continuing.");
+    }
+});
+
+
+Route::get('/get', function () {
+
+    $url = 'http://s3.' . env('AWS_DEFAULT_REGION') . '.amazonaws.com/' . env('AWS_BUCKET') . '/';
+    try {
+        $images = [];
+        $files = Storage::disk('s3')->files('files');
+        foreach ($files as $file) {
+            $urltemp = Storage::temporaryUrl(
+                $file,
+                now()->addMinutes(5)
+            );
+            $images[] = [
+                'name' => str_replace('files/', '', $file),
+                'src' => $urltemp
+                // 'src' => $url . $file
+            ];
+            if (Str::contains($file, '.jpg')) {
+                echo '<img src="' . $urltemp . '"/>';
+                echo '<a href="'.$urltemp.'" download>Baixar</a>';
+                // return Storage::download($file,'teste55.jpg');
+            }
+        }
+
+        // echo '<pre>'; print_r($images);echo '</pre>';
+
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
+        exit("Please fix error with file upload before continuing.");
+    }
+});
